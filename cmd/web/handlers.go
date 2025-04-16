959a21e5711d82522e819e34888810f3cffb81af
andrewcara <64450522+andrewcara@users.noreply.github.com>
2025-04-16 13:49:54 -0400

    Adding environment variables to template in go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+	pk_key := os.Getenv("pk_test")
+	data := map[string]interface{}{
+		"pk_key": pk_key,
+	}
+	td := &templateData{Data: data}
+
+	if err := app.renderTemplate(w, r, "terminal", td); err != nil {
 		app.errorLog.Println(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/andrewcara/go-stripe.git
 
 go 1.23.5
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/stripe/stripe-go/v72 v72.122.0 // indirect
+)
